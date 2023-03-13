cd906a56d090a2e00b43ed79014fed0b6041ed82
Wajid Khan <wajid.developer1@gmail.com>
2023-03-13 17:51:59 +0530

    Project Service code updated

--- src/views/services/projectservices.js
+++ src/views/services/projectservices.js
@@ -1,68 +1,57 @@
 import { endpoints } from "../constants/endpoints";
-import axios from "axios";
-let config = {
-    headers: {
-        APICODE: "token",
-        headers: {
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS,HEAD",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Content-Type": "application/json",
-        },
-    },
-};
+import fetch from "../../fetchinterceptor";
 
 const ProjectService = {};
 
 ProjectService.getproject = () =>
-    axios
-        .get(
-            `${endpoints.getprojects}`,
-            config
-        )
-        .then((response) => {
-            return response;
-        })
-        .catch((error) => { return error });
 
-ProjectService.getprojectbyid = (id) => {
-    
-    axios
-        .get(
-            `${endpoints.getprojectbyid}${id}`,
-            config
-        )
-        .then((response) => {
-            return response;
+    fetch({
+            url: `${endpoints.getprojects}`,
+            method: "get",
         })
-        .catch((error) => { return error });
-}
+            .then((data) => {
+                return data;
+            })
+            .catch((err) => {
+                return err;
+            });
+
+ProjectService.getprojectbyid = (id) => 
+    
+    fetch({
+        url: `${endpoints.getprojectbyid}${id}`,
+        method: "get",
+    })
+        .then((data) => {
+            return data;
+        })
+        .catch((err) => {
+            return err;
+        });
+
     
 ProjectService.addproject = (obj) =>
-    axios
-        .post(
-            `${endpoints.addproject}`,
-            obj,
-            config
-        )
-        .then((response) => {
-            return response;
+
+    fetch
+        .post(`${endpoints.addproject}`, obj)
+        .then((data) => {
+            return data;
         })
-        .catch((error) => { return error });
+        .catch((err) => {
+            return err;
+        });
 
 
 ProjectService.editproject = (obj) =>
-    axios
-        .post(
-            `${endpoints.editproject}`,
-            obj,
-            config
-        )
-        .then((response) => {
-            return response;
+    
+    fetch
+        .post(`${endpoints.editproject}`, obj)
+        .then((data) => {
+            return data;
         })
-        .catch((error) => { return error });
+        .catch((err) => {
+            return err;
+        });
 
 
 export default ProjectService;
