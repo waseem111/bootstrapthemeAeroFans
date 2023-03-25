e942a6bf75a54b1fb1bdf1f8d8022a953192b733
waseem111 <55344039+waseem111@users.noreply.github.com>
2023-03-25 15:21:21 +0530

    loader css

--- src/views/app/dashboard/dashboard.js
+++ src/views/app/dashboard/dashboard.js
@@ -14,8 +14,8 @@ const Dashboard = () => {
             <div className='loader-inner-main'>
                     <Vortex
   visible={true}
-  height="100"
-  width="100"
+  height="90"
+  width="90"
   ariaLabel="vortex-loading"
   wrapperStyle={{}}
   wrapperClass="vortex-wrapper"
