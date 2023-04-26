d7825f8428421dd33cb2bddf39c8d0085dd5b636
Zeeshan-Wassma <Zeeshan@ibsksa.com>
2023-04-26 11:57:38 +0530

    Update global.js

--- src/views/constants/global.js
+++ src/views/constants/global.js
@@ -1,7 +1,7 @@
 export const global = {
-    //api_url: "http://52.202.7.252:3007/api/",
-    // fandata_api_url: "http://3.109.124.68/"
-    api_url: "http://localhost:3007/api/"
+    api_url: "http://52.202.7.252:3007/api/",
+    fandata_api_url: "http://3.109.124.68/"
+    //api_url: "http://localhost:3007/api/"
 }
 
 //api_url: "http://52.202.7.252:3007/api/",
